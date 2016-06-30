4700e8ae59145442005d8b5c1965f15d5853168c
ericmvasey <ericmvasey@comcast.net>
2016-06-30 18:46:45 -0400

    Added IP dialog. Added file picker dialog for playing music.

--- app/src/main/java/edu/mtu/naoremote/MainActivity.java
+++ app/src/main/java/edu/mtu/naoremote/MainActivity.java
@@ -1,7 +1,13 @@
 package edu.mtu.naoremote;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,14 +16,25 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.aldebaran.qi.Application;
 import com.aldebaran.qi.Session;
+import com.aldebaran.qi.Tuple10;
+import com.aldebaran.qi.Tuple2;
+import com.aldebaran.qi.Tuple20;
+import com.aldebaran.qi.Tuple3;
+import com.aldebaran.qi.Tuple5;
 import com.aldebaran.qi.helper.proxies.ALAnimatedSpeech;
+import com.aldebaran.qi.helper.proxies.ALAudioPlayer;
 import com.aldebaran.qi.helper.proxies.ALMotion;
 import com.aldebaran.qi.helper.proxies.ALRobotPosture;
 import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+import com.aldebaran.qi.helper.proxies.PackageManager;
 
+import java.lang.reflect.Array;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -26,11 +43,18 @@ public class MainActivity extends AppCompatActivity
     private ALMotion motion;
     private ALRobotPosture posture;
     private ALAnimatedSpeech animatedSpeech;
+    private ALAudioPlayer audioPlayer;
+    private PackageManager packageManager;
 
-    private Button say;
-    private Spinner postureSelector;
+    private Button say, playSound, stopSound;
+    private Spinner postureSelector, packageSelector;
     private CheckBox autoAnimate;
     private EditText textToSay;
+
+    private static final int AUDIO_FILE_REQUEST_CODE = 4559;
+
+    private String robotUrl = "141.219.121.74:9559";
+    private int soundID = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -38,7 +62,46 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String robotUrl = "141.219.121.74:9559";
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View v = getLayoutInflater().inflate(R.layout.dialog_pickip, null, false);
+        EditText ipAddress = (EditText) v.findViewById(R.id.ipAddress);
+        ipAddress.setText(robotUrl);
+
+        builder.setView(v);
+        builder.setCancelable(false);
+
+        builder.setMessage("Enter NAO's IP Address")
+                .setTitle("IP Address");
+
+        builder.setPositiveButton("Connect", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Dialog d = (Dialog) dialog;
+                EditText ipAddress = (EditText) d.findViewById(R.id.ipAddress);
+
+                robotUrl = ipAddress.getText().toString();
+                init();
+            }
+        });
+
+        builder.show();
+
+
+        autoAnimate = (CheckBox) findViewById(R.id.enableAutoGestures);
+        textToSay = (EditText) findViewById(R.id.textToSay);
+        postureSelector = (Spinner) findViewById(R.id.poseSpinner);
+        packageSelector = (Spinner) findViewById(R.id.packageSpinner);
+        say = (Button) findViewById(R.id.say);
+        playSound = (Button) findViewById(R.id.playSample);
+        stopSound = (Button) findViewById(R.id.stopSample);
+    }
+
+    private void init()
+    {
+
         try
         {
             session = new Session();
@@ -47,13 +110,11 @@ public class MainActivity extends AppCompatActivity
             motion = new ALMotion(session);
             posture = new ALRobotPosture(session);
             animatedSpeech = new ALAnimatedSpeech(session);
+            packageManager = new PackageManager(session);
+            audioPlayer = new ALAudioPlayer(session);
+
             animatedSpeech.setBodyLanguageModeFromStr("contextual");
 
-
-            autoAnimate = (CheckBox) findViewById(R.id.enableAutoGestures);
-            textToSay = (EditText) findViewById(R.id.textToSay);
-
-            say = (Button) findViewById(R.id.say);
             say.setOnClickListener(new View.OnClickListener()
             {
                 @Override
@@ -75,9 +136,6 @@ public class MainActivity extends AppCompatActivity
             });
 
             List<String> postures = posture.getPostureList();
-
-            postureSelector = (Spinner) findViewById(R.id.poseSpinner);
-
             ArrayAdapter<String> postureAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, (String[]) postures.toArray());
             postureSelector.setAdapter(postureAdapter);
 
@@ -98,10 +156,76 @@ public class MainActivity extends AppCompatActivity
                     }
                 }
             });
+
+            playSound.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    try
+                    {
+                        if(soundID != -1 && audioPlayer.isRunning(soundID));
+                        {
+                            audioPlayer.stop(soundID);
+                        }
+
+                        Intent pickFile = new Intent(Intent.ACTION_GET_CONTENT);
+                        pickFile.setType("file/*");
+                        startActivityForResult(pickFile, AUDIO_FILE_REQUEST_CODE);
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            stopSound.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    try
+                    {
+                        if(soundID != -1 && audioPlayer.isRunning(soundID));
+                        {
+                            audioPlayer.stop(soundID);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            List<String> packages = (List<String>) packageManager.getPackages();
+            ArrayAdapter<String> packagesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, packages);
+            packageSelector.setAdapter(packagesAdapter);
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode == AUDIO_FILE_REQUEST_CODE)
+        {
+            String path = data.getData().getPath();
+
+            try
+            {
+                audioPlayer.playFile(path);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        else
+            super.onActivityResult(requestCode, resultCode, data);
     }
 }
