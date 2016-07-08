a3004043867d86f99a7bd7266d9d29f2c4ba06f7
ericmvasey <ericmvasey@comcast.net>
2016-07-08 02:36:44 -0400

    Added connection dialog. Added test code to transfer audio files to NAO usinc SCP.

--- app/src/main/java/edu/mtu/naoremote/MainActivity.java
+++ app/src/main/java/edu/mtu/naoremote/MainActivity.java
@@ -1,40 +1,43 @@
 package edu.mtu.naoremote;
 
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
+import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.aldebaran.qi.Application;
 import com.aldebaran.qi.Session;
-import com.aldebaran.qi.Tuple10;
-import com.aldebaran.qi.Tuple2;
-import com.aldebaran.qi.Tuple20;
-import com.aldebaran.qi.Tuple3;
-import com.aldebaran.qi.Tuple5;
 import com.aldebaran.qi.helper.proxies.ALAnimatedSpeech;
 import com.aldebaran.qi.helper.proxies.ALAudioPlayer;
 import com.aldebaran.qi.helper.proxies.ALMotion;
 import com.aldebaran.qi.helper.proxies.ALRobotPosture;
 import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
 import com.aldebaran.qi.helper.proxies.PackageManager;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.UserInfo;
 
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -46,6 +49,9 @@ public class MainActivity extends AppCompatActivity
     private ALAudioPlayer audioPlayer;
     private PackageManager packageManager;
 
+    private JSch jSch;
+    private com.jcraft.jsch.Session sshSession;
+
     private Button say, playSound, stopSound;
     private Spinner postureSelector, packageSelector;
     private CheckBox autoAnimate;
@@ -54,42 +60,21 @@ public class MainActivity extends AppCompatActivity
     private static final int AUDIO_FILE_REQUEST_CODE = 4559;
 
     private String robotUrl = "141.219.121.74:9559";
+    private String robotSSHUsername = "nao";
+    private String robotSSHPassword = "";
     private int soundID = -1;
 
+    private SharedPreferences preferences;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void onCreate(final Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        preferences = getPreferences(MODE_PRIVATE);
 
-        View v = getLayoutInflater().inflate(R.layout.dialog_pickip, null, false);
-        EditText ipAddress = (EditText) v.findViewById(R.id.ipAddress);
-        ipAddress.setText(robotUrl);
-
-        builder.setView(v);
-        builder.setCancelable(false);
-
-        builder.setMessage("Enter NAO's IP Address")
-                .setTitle("IP Address");
-
-        builder.setPositiveButton("Connect", new DialogInterface.OnClickListener()
-        {
-            @Override
-            public void onClick(DialogInterface dialog, int which)
-            {
-                Dialog d = (Dialog) dialog;
-                EditText ipAddress = (EditText) d.findViewById(R.id.ipAddress);
-
-                robotUrl = ipAddress.getText().toString();
-                init();
-            }
-        });
-
-        builder.show();
-
-
+        //Get views
         autoAnimate = (CheckBox) findViewById(R.id.enableAutoGestures);
         textToSay = (EditText) findViewById(R.id.textToSay);
         postureSelector = (Spinner) findViewById(R.id.poseSpinner);
@@ -97,6 +82,77 @@ public class MainActivity extends AppCompatActivity
         say = (Button) findViewById(R.id.say);
         playSound = (Button) findViewById(R.id.playSample);
         stopSound = (Button) findViewById(R.id.stopSample);
+
+        connectionDialog();
+    }
+
+    private void connectionDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View v = getLayoutInflater().inflate(R.layout.dialog_pickip, null, false);
+        final EditText ipAddress = (EditText) v.findViewById(R.id.ipAddress);
+        final EditText sshUname = (EditText) v.findViewById(R.id.sshUsername);
+        final EditText sshPass = (EditText) v.findViewById(R.id.sshPassword);
+        final CheckBox savePassword = (CheckBox) v.findViewById(R.id.savePassword);
+        final CheckBox showPassword =  (CheckBox) v.findViewById(R.id.showPassword);
+
+        showPassword.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+
+                if(isChecked)
+                {
+                    sshPass.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    sshPass.setSelection(sshPass.length());
+                }
+                else
+                {
+                    sshPass.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    sshPass.setSelection(sshPass.length());
+                }
+            }
+        });
+
+        ipAddress.setText(robotUrl);
+        sshUname.setText(robotSSHUsername);
+
+        if(preferences.contains("savedPassword"))
+        {
+            sshPass.setText(preferences.getString("savedPassword", ""));
+            savePassword.setChecked(true);
+        }
+
+        builder.setView(v);
+        builder.setCancelable(false);
+
+        builder.setTitle("Connect to NAO");
+
+        builder.setPositiveButton("Connect", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+
+                robotUrl = ipAddress.getText().toString();
+                robotSSHUsername = sshUname.getText().toString();
+                robotSSHPassword = sshPass.getText().toString();
+
+                if(savePassword.isChecked())
+                    preferences.edit().putString("savedPassword", robotSSHPassword).apply();
+                else
+                {
+                    if(preferences.contains("savedPassword"))
+                        preferences.edit().remove("savedPassword").apply();
+                }
+
+                init();
+            }
+        });
+
+        builder.show();
     }
 
     private void init()
@@ -112,6 +168,52 @@ public class MainActivity extends AppCompatActivity
             animatedSpeech = new ALAnimatedSpeech(session);
             packageManager = new PackageManager(session);
             audioPlayer = new ALAudioPlayer(session);
+
+            jSch = new JSch();
+            sshSession = jSch.getSession("nao", String.valueOf(robotUrl), 22);
+            UserInfo info = new UserInfo()
+            {
+                @Override
+                public String getPassphrase()
+                {
+                    return null;
+                }
+
+                @Override
+                public String getPassword()
+                {
+                    return robotSSHPassword;
+                }
+
+                @Override
+                public boolean promptPassword(String message)
+                {
+                    return true;
+                }
+
+                @Override
+                public boolean promptPassphrase(String message)
+                {
+                    return false;
+                }
+
+                @Override
+                public boolean promptYesNo(String message)
+                {
+                    return false;
+                }
+
+                @Override
+                public void showMessage(String message)
+                {
+                    Log.d("SshMessage", message);
+                }
+            };
+
+            sshSession.setUserInfo(info);
+            sshSession.connect();
+
+            if(sshSession.isConnected())
 
             animatedSpeech.setBodyLanguageModeFromStr("contextual");
 
@@ -164,13 +266,13 @@ public class MainActivity extends AppCompatActivity
                 {
                     try
                     {
-                        if(soundID != -1 && audioPlayer.isRunning(soundID));
+                        /*if(soundID != -1 && audioPlayer.isRunning(soundID))
                         {
                             audioPlayer.stop(soundID);
-                        }
+                        }*/
 
                         Intent pickFile = new Intent(Intent.ACTION_GET_CONTENT);
-                        pickFile.setType("file/*");
+                        pickFile.setType("audio/*");
                         startActivityForResult(pickFile, AUDIO_FILE_REQUEST_CODE);
                     }
                     catch(Exception e)
@@ -187,7 +289,7 @@ public class MainActivity extends AppCompatActivity
                 {
                     try
                     {
-                        if(soundID != -1 && audioPlayer.isRunning(soundID));
+                        if(soundID != -1 && audioPlayer.isRunning(soundID))
                         {
                             audioPlayer.stop(soundID);
                         }
@@ -214,15 +316,61 @@ public class MainActivity extends AppCompatActivity
     {
         if(requestCode == AUDIO_FILE_REQUEST_CODE)
         {
-            String path = data.getData().getPath();
+            if(resultCode == RESULT_OK)
+            {
+                try
+                {
+                    File toSend = new File(data.getData().getPath());
+                    String receiveCommand = "scp -p -t " + toSend.getName();
+                    Channel channel = sshSession.openChannel("exec");
+                    ((ChannelExec) channel).setCommand(receiveCommand);
 
-            try
-            {
-                audioPlayer.playFile(path);
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
+                    OutputStream out = channel.getOutputStream();
+                    InputStream in = channel.getInputStream();
+
+                    channel.connect();
+
+                    //Date last modified
+                    out.write( ("T " + toSend.lastModified()/1000 + "0\n").getBytes() );
+                    out.flush();
+
+                    //File Size
+                    out.write(("C0644 " + toSend.length() + "\n").getBytes());
+                    out.flush();
+
+                    //Send File
+                    FileInputStream fis = new FileInputStream(toSend);
+                    byte[] buffer = new byte[1024];
+
+                    while(true)
+                    {
+                        int len = fis.read(buffer, 0, buffer.length);
+
+                        if(len <= 0)
+                            break;
+
+                        out.write(buffer, 0, len);
+                    }
+
+                    fis.close();
+
+                    //Null terminator
+                    buffer[0] = 0;
+                    out.write(buffer, 0, 1);
+                    out.flush();
+                    out.close();
+
+                    //Disconnect from exec channel
+                    channel.disconnect();
+
+
+
+                    //audioPlayer.playFile(path);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
             }
         }
         else
